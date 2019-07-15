52f1e04b690c85311b20aa3129e03f91bab5308b
Ehsaan Iqbal <ishu283@gmail.com>
2019-07-15 22:53:19 +0400

    Production ready [v1]

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 //cors 
@@ -16,6 +17,13 @@ app.use(express.json({extended:false}));
 app.use('/', require('./routes/index'));
 app.use('/api', require('./routes/url'));
 
+//serve static assets 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 
 
